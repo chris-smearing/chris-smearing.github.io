cdd2d17e2369a305da0079b48a89c908deeb105c
Chris Smearing <chris.smearing@gmail.com>
2019-11-21 12:31:03 -0600

    Added affiliates
    added multiple card transition effects;
    added better method of gulp watch and reload

--- gulpfile.js
+++ gulpfile.js
@@ -30,18 +30,21 @@ function scssTask(){
 function jsTask(){
     return src(['js/*.min.js', '!js/bundle.min.js'])
         .pipe(concat('bundle.min.js'))
-        .pipe(dest('js')
-    );
+        .pipe(dest('js'))
+        .pipe(browsersync.reload({ stream: true }));
+}
+
+function pageTask(){
+    return src('index.html')
+    .pipe(browsersync.reload({ stream: true }));
 }
 
 // Watch task: watch SCSS and JS files for changes
 // If any change, run scss and js tasks simultaneously
 function watchTask(){
-    watch(['scss/**/*.scss', 'js/**/*.min.js', '!js/bundle.min.js'], 
-        series(
-            parallel(scssTask, jsTask),
-        )
-    );    
+    watch('scss/**/*.scss', series(scssTask));
+    watch(['js/**/*.min.js', '!js/bundle.min.js'], series(jsTask));
+    watch('index.html', series(pageTask));
 }
 
 function browserSync(done) {
